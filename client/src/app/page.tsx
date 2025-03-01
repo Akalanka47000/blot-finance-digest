30ff75fe62b8388df1b22d4a5ed48ec6dc47c312
Akalanka Perera <akalankaperera128@gmail.com>
2025-03-01 17:43:54 +0530

    Fix: added revalidation time as 60secs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { BlottPosts, FinnhubPosts, Hero } from '@/components/home';
 
+export const revalidate = 60;
+
 export default function Page(): JSX.Element {
   return (
     <main className="grow px-5 md:px-10">
