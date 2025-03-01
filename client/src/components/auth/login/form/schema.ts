20aea42be5c376aa04dbb675e77ba4a78eac949e
Akalanka Perera <akalankaperera128@gmail.com>
2025-03-01 15:33:59 +0530

    Fix: cookie same site

--- client/src/components/auth/login/form/schema.ts
+++ client/src/components/auth/login/form/schema.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
-export default z
-  .object({
-    email: z.string().email(),
-    password: z.string().min(1, "Password is required")
-  })
+export default z.object({
+  email: z.string().email(),
+  password: z.string().min(1, 'Password is required')
+});
